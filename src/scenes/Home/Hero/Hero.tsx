a0be85b670048641c2d5a4625dafa8b692d03878
YuriiPohorilets <pohorilets.y@gmail.com>
2024-06-08 14:17:09 +0300

    add. home page hero content

--- src/scenes/Home/Hero/Hero.tsx
+++ src/scenes/Home/Hero/Hero.tsx
@@ -1,17 +1,36 @@
-import Image from 'next/image';
-
 import { Container, Section } from '@/components/common';
+import { Benefits } from '@/modules';
+import { CrownIcon } from '@/assets/icons';
 
 import styles from './Hero.module.scss';
 
+const content = {
+  title: ['Bachelor bunny', 'gourmet'],
+  description: 'Delicious meals delivered to your door',
+};
+
 export const Hero: React.FC = () => {
   return (
-    <>
-      <Section shadow="bottom">
-        <Container>
-          <div className={styles.wrapper}></div>
-        </Container>
-      </Section>
-    </>
+    <Section shadow="bottom">
+      <Container>
+        <div className={styles.wrapper}>
+          <div className={styles.contentWrapper}>
+            <div className={styles.titleWrapper}>
+              <div className={styles.iconWrapper}>
+                <CrownIcon />
+              </div>
+
+              <h1 className={styles.title}>
+                {content.title[0]} <span>{content.title[1]}</span>
+              </h1>
+            </div>
+
+            <p className={styles.description}>{content.description}</p>
+          </div>
+
+          <Benefits />
+        </div>
+      </Container>
+    </Section>
   );
 };
