6fa39c401202d7eb6bd59b966ed92313e19f1a3e
YuriiPohorilets <pohorilets.y@gmail.com>
2024-06-17 22:37:17 +0300

    add. about_page - hero

--- src/pages/about-us/index.tsx
+++ src/pages/about-us/index.tsx
@@ -1,5 +1,25 @@
+import { HeadPage } from '@/components/common';
+import { BackgroundWrapper } from '@/components/misc';
+import { Footer } from '@/modules';
+import { Hero } from '@/scenes/AboutUs';
+
+const headContent = {
+  title: 'About us - Bachelor Bunny Gourmet',
+  description: 'Crafting gourmet delights, personalized for your lifestyle',
+};
+
 const AboutUsPage = () => {
-  return <div>About us page</div>;
+  return (
+    <>
+      <HeadPage title={headContent.title} description={headContent.description} />
+
+      <Hero />
+
+      <BackgroundWrapper bgUrl={''}>
+        <Footer />
+      </BackgroundWrapper>
+    </>
+  );
 };
 
 export default AboutUsPage;
--- src/scenes/AboutUs/Hero/Hero.tsx
+++ src/scenes/AboutUs/Hero/Hero.tsx
@@ -1,5 +1,26 @@
-import React from 'react';
+import { Container, Section } from '@/components/common';
 
-export const Hero = () => {
-  return <div>Hero</div>;
+import styles from './Hero.module.scss';
+
+const content = {
+  title: 'Sophia Jennings',
+  subtitle: 'Owner & Head chef',
+  description: 'Crafting gourmet delights, personalized for your lifestyle',
+};
+
+export const Hero: React.FC = () => {
+  return (
+    <Section className={styles.section}>
+      <Container>
+        <div className={styles.wrapper}>
+          <div className={styles.titleWrapper}>
+            <h1 className={styles.title}>{content.title}</h1>
+            <span className={styles.subtitle}>{content.subtitle}</span>
+          </div>
+
+          <p className={styles.description}>{content.description}</p>
+        </div>
+      </Container>
+    </Section>
+  );
 };
